7e9d6a804c3a027e6dd84456fd755704ee4c8e8d
TSBA\Taylor <taylorb@tds.net>
2017-05-13 14:29:00 -0400

    cleaned up redundant code and commented on old code for clarity

--- game_finaltayedit.py
+++ game_finaltayedit.py
@@ -4,42 +4,52 @@ from pygame import display
 
 #LIST OF ANCHORS TO USE
 my_list = list()
+#gives a number of tiles to use for loop
 number_of_tiles = 8
+#first tile x value to use for incrementing 
 p=550
+#number that determines number of tiles in a row (see inner if statement)
 n=0
+#first tile y value for tiles 
 o = 350
+#puts x and y value into set for the list appending
 m = (p,o)
+#adds set to list
 my_list.append(m)
+#loop value for generating tile space
 q=0
 while (number_of_tiles - 1 > q):
+    #increment value for x increase for wider gap decrease for narrower
     p +=150
+    #increments the column value
     n += 1
+    # the value you set n equal to will determine the ammount of columns
     if (n==4):
-        o=500
+        #set as new y value for second row can be entered to give more rows
+        o += 150
+        #resets x value for tile placement in new row change to same value above of the x firts tile
         p = 550
-        n= -4
-    print(n)
-
+        #resets n so that new  rows will be able to be made
+        n= 0
+    #adds iteration to loop
     q +=1
+    #takes new values and puts them in list
     m = (p,o)
     my_list.append(m)
-    print(q)
-#my_list = ((600 + 100,350), (750+ 100,350),(900+ 100,350),(450+ 100,350),(450+ 100,500),(900+ 100,500),(750+ 100,500),(600+ 100,500))
+#puts starting list in new format
 x = list(my_list)
+#shuffles values to randomize values
 random.shuffle(x)
-print (my_list)
-print(x)
-
 #colors
 BLUE = (0,70,200)
 
-LIGHTOFF =('LightOff.png')
-LIGHTON = ('LightOn.png')
-LIGHT =('Light.png')
-
+#vairiables 
 WIN = False
 ATTEMPTS = 12
 SCORE = 0
+
+    
+
 #dimensions
 TILESIZE = 100
 MAPWIDTH = 1
@@ -47,6 +57,7 @@ MAPHEIGHT = 1
 WINDOWWIDTH = 1520
 WINDOWHEIGHT = 930
 
+#loads tiles to value tags to be utilized
 BLANK = ('tileBack.png')
 ONE = ('tileFront0.png')
 ONEB = ('tileFront00.png')
@@ -57,7 +68,11 @@ THREEB = ('tileFront10.png')
 FOUR = ('tileFront3.png')
 FOURB = ('tileFront11.png')
 BACKGROUND = ('tilEBackground.png')
-##
+LIGHTOFF =('LightOff.png')
+LIGHTON = ('LightOn.png')
+LIGHT =('Light.png')
+
+#generates the score sprite and updates
 class Score(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -75,14 +90,13 @@ class Score(pygame.sprite.Sprite):
             self.lastscore = SCORE
             msg = "Score: %d" % SCORE
             self.image = self.font.render(msg, 0, self.color)
-
+            
+#generates the attempts sprites and updates for it
 class Attempts(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        #self.font = pygame.font.SysFont("None", 40)
         pygame.font.init()
-        #Font= pygame.font.Font("TemploGordo.ttf", 40)
         self.font = pygame.font.SysFont("None", 25)
 
         self.font.set_italic(0)
@@ -96,19 +110,20 @@ class Attempts(pygame.sprite.Sprite):
             self.lastattempts = ATTEMPTS
             msg = "Attempts: %d" % ATTEMPTS
             self.image = self.font.render(msg, 0, self.color)
-
-def main(winstyle=0):
-    #set up display
+#main loop
+def main():
+    #initilizes game
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    winstyle = 0
+    #sets up display and adds title to the border
+    screen = pygame.display.set_mode((1520, 930))
+    pygame.display.set_caption('Binary Memory Game')
+    #initilizes fint fo game
     pygame.font.init()
     myfont = pygame.font.SysFont("None", 25)
-    #myfont = pygame.font.SysFont("None", 30)
-    #DISPLAYSURF = pygame.image.load(BACKGROUND)
+    #sest winner vairibale for loop
     winner = True
     
+    #loads in images and assigns them to new image vairiables to be used
     background = pygame.image.load(BACKGROUND).convert_alpha()
     blank =pygame.image.load(BLANK).convert_alpha()
     onenum =pygame.image.load(ONE).convert_alpha()
@@ -123,17 +138,17 @@ def main(winstyle=0):
     lightOff=pygame.image.load(LIGHT).convert_alpha()
     lightWrong =pygame.image.load(LIGHTOFF).convert_alpha()
     lightOff2=pygame.image.load(LIGHT).convert_alpha()
-         
+    
+    #gets mouse values
     mousex = 0 # used to store x coordinate of mouse event
     mousey = 0 # used to store y coordinate of mouse event
-    pygame.display.set_caption('Binary Memory Game')
     
+    #each screen.blit adds an image to the screen for the user to see
     screen.blit(background,(0,0))
     screen.blit(lightOn,(30,280))
     screen.blit(lightWrong,(30,520))
     screen.blit(lightOff,(30,280))
     screen.blit(lightOff2, (30, 520))
-    print(x[0])
     screen.blit(onenum,x[0]) 
     screen.blit(onebin,x[1])
     screen.blit(twonum,x[2])  
@@ -142,6 +157,16 @@ def main(winstyle=0):
     screen.blit(threebin,x[5])  
     screen.blit(fournum,x[6])  
     screen.blit(fourbin,x[7])
+        
+    #adds the blank tiles on top of the numbers
+    screen.blit(blank,x[0])  
+    screen.blit(blank,x[1])
+    screen.blit(blank,x[2])  
+    screen.blit(blank,x[3])  
+    screen.blit(blank,x[4])  
+    screen.blit(blank,x[5])  
+    screen.blit(blank,x[6])  
+    screen.blit(blank,x[7])
 
     #Test new score draw method
     all = pygame.sprite.RenderUpdates()
@@ -156,10 +181,11 @@ def main(winstyle=0):
         all.add(Score())
         all.add(Attempts())
 
-
+    #gives a list of unzipped values for the x and y to be used for individual placement
     y = x
     posa, posb = zip(*y)
     
+    #takes unzipped values to make clickable rectangles on the images the 100's are pixel size
     onenpos = Rect(posa[0], posb[0], 100, 100)
     onebpos = Rect(posa[1], posb[1], 100, 100)     
     twonpos = Rect(posa[2], posb[2], 100, 100)     
@@ -169,15 +195,7 @@ def main(winstyle=0):
     fournpos = Rect(posa[6], posb[6], 100, 100)     
     fourbpos = Rect(posa[7], posb[7], 100, 100)
     
-    screen.blit(blank,x[0])  
-    screen.blit(blank,x[1])
-    screen.blit(blank,x[2])  
-    screen.blit(blank,x[3])  
-    screen.blit(blank,x[4])  
-    screen.blit(blank,x[5])  
-    screen.blit(blank,x[6])  
-    screen.blit(blank,x[7])
-    
+    #adds boolean for each tile if they have been clicked or not 
     oneClick = True
     onebClick = True
     twoClick = True
@@ -187,14 +205,15 @@ def main(winstyle=0):
     fourClick = True
     fourbClick = True
     
-    tally_list = list() 
-    totalcounter = 0
-    first_click = 5000
-    second_click = 6000
-    click_counter = 0
-    miss = False
+    #sets the light to off
     lightactive = False
-    
+    #counter for the tiles to determine if two have been clicked
+    click_counter = 0
+    #sets the miss bool 
+    miss = False
+    #user events list to hold clicked values
+    clickL= list()
+        
     #The current place for instructions. This will be replaced with a png later
     instructions = myfont.render('Match the binary numbers', True, (235,255,255))
     instruct2 = myfont.render( 'with their corresponding', True, (235, 255, 255))
@@ -203,9 +222,7 @@ def main(winstyle=0):
     screen.blit(instruct2,(200,400))
     screen.blit(instruct3,(200, 430))
 
-    #user events
-    clickL= list()
-    
+    #loop to run game while the player hasnt expended all attempts
     while (ATTEMPTS>=0):
         # clear/erase the last drawn sprites
         all.clear(screen, background)
@@ -220,6 +237,7 @@ def main(winstyle=0):
             time.sleep(.2)
             screen.blit(lightOff,(30,520))
             screen.blit(lightOff,(30,280))
+        #checks to see if an event has happened
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -232,75 +250,60 @@ def main(winstyle=0):
                 mouse_pos = event.pos
                 mousex, mousey = event.pos
                 mouseClicked = True
+                #after a mouse event has happened checks to see if a image was clicked
                 if onenpos.collidepoint(mouse_pos):
                     if (oneClick == True):
                         clickL.append(1)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('1')
                         oneClick = False
                         screen.blit(onenum,x[0]) 
                 if onebpos.collidepoint(mouse_pos):
                     if (onebClick == True):
                         clickL.append(1)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('1')
                         onebClick = False
                         screen.blit(onebin,x[1])
                 if twonpos.collidepoint(mouse_pos):
                     if (twoClick == True):
                         clickL.append(2)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('2')
                         twoClick = False
                         screen.blit(twonum,x[2]) 
                 if twobpos.collidepoint(mouse_pos):
                     if (twobClick == True):
                         clickL.append(2)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('2')
                         twobClick = False
                         screen.blit(twobin,x[3])
                 if threenpos.collidepoint(mouse_pos):
                     if (threeClick == True):
                         clickL.append(3)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('3')
                         threeClick = False
                         screen.blit(threenum,x[4]) 
                 if threebpos.collidepoint(mouse_pos):
                     if (threebClick == True):
                         clickL.append(3)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('3')
                         threebClick = False
                         screen.blit(threebin,x[5])
                 if fournpos.collidepoint(mouse_pos):
                     if (fourClick == True):
                         clickL.append(4)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('4')
                         fourClick = False
                         screen.blit(fournum,x[6]) 
                 if fourbpos.collidepoint(mouse_pos):
                     if (fourbClick == True):
                         clickL.append(4)
                         click_counter +=1
-                        totalcounter += 1
-                        print ('4')
                         fourbClick = False
                         screen.blit(fourbin,x[7])
 
-      
                 pygame.display.update()
-                
+            # after two images has been clicked enters   
             if (click_counter == 2) :  
+                #if they matched then proceds to make the tiles unclickable for the remainder of the game
                 if (clickL[0] == clickL[1]):
                     if (clickL[0] == 1):
                         oneClick = False
@@ -314,6 +317,7 @@ def main(winstyle=0):
                     if (clickL[0]  == 4):
                         fourClick = False
                         fourbClick = False
+                    #adds score and removes attempt as well as flashes the green light and resets the list of player clicks
                     SCORE += 1
                     ATTEMPTS -= 1
                     click_counter = 0
@@ -322,139 +326,43 @@ def main(winstyle=0):
                     screen.blit(lightOn,(30,520))
                     time.sleep(.5)
                     lightactive = True
-                elif (first_click is not second_click):
+                #if the player clicked two wron tiles removes attempt and flashes red light
+                elif (clickL[0]  is not clickL[1] ):
                     ATTEMPTS -= 1
-                    time.sleep(.5)
                     lightactive = True
                     screen.blit(lightWrong,(30,280))
+                    time.sleep(.5)
                     miss = True
+                    #after miss will reset the clickability of the tile and flips it back over
                     if (miss == True):
-                        if (clickL[0] == 1 and clickL[1] == 2):
+                        if (clickL[0] == 1 or clickL[1] == 1):
                             oneClick = True
                             onebClick = True
-                            twoClick = True
-                            twobClick = True
                             screen.blit(blank,x[0])  
                             screen.blit(blank,x[1])
+                        if (clickL[0]  == 2 or clickL[1] == 2):
+                            twoClick = True
+                            twobClick = True
                             screen.blit(blank,x[2])  
                             screen.blit(blank,x[3])
-                            print('inside')
-                        elif (clickL[0]  == 2 and clickL[1] == 1):
-                            oneClick = True
-                            onebClick = True
-                            twoClick = True
-                            twobClick = True
-                            screen.blit(blank,x[0])  
-                            screen.blit(blank,x[1])
-                            screen.blit(blank,x[2])  
-                            screen.blit(blank,x[3])
-                            print('inside')
-                        elif (clickL[0]  == 1 and clickL[1] == 3):
-                            screen.blit(blank,x[0])  
-                            screen.blit(blank,x[1])
+                        if (clickL[0]  == 3 or clickL[1] == 3):
                             screen.blit(blank,x[4])  
                             screen.blit(blank,x[5])
-                            oneClick = True
-                            onebClick = True
                             threeClick = True
                             threebClick = True
-                        elif (clickL[0]  == 3 and clickL[1] == 1):
-                            screen.blit(blank,x[0])
-                            screen.blit(blank,x[1])
-                            screen.blit(blank,x[4])  
-                            screen.blit(blank,x[5])
-                            oneClick = True
-                            onebClick = True
-                            threeClick = True
-                            threebClick = True
-                            print('inside')
-                        elif (clickL[0]  == 1 and clickL[1] == 4):
-                            oneClick = True
-                            onebClick = True
+                        if (clickL[0]  == 4 or clickL[1] == 4):
                             fourClick = True
                             fourbClick = True
-                            screen.blit(blank,x[0])
-                            screen.blit(blank,x[1])
                             screen.blit(blank,x[6])
                             screen.blit(blank,x[7])
-                            print('inside')
-                        elif (clickL[0]  == 4 and clickL[1] == 1):
-                            oneClick = True
-                            onebClick = True
-                            fourClick = True
-                            fourbClick = True
-                            screen.blit(blank,x[0])
-                            screen.blit(blank,x[1])
-                            screen.blit(blank,x[6])
-                            screen.blit(blank,x[7])
-                            print('inside')
-                        elif (clickL[0]  == 2 and clickL[1] == 3):
-                            twoClick = True
-                            twobClick = True
-                            threeClick = True
-                            threebClick = True
-                            screen.blit(blank,x[2])
-                            screen.blit(blank,x[3])
-                            screen.blit(blank,x[4])
-                            screen.blit(blank,x[5])
-                            print('inside')
-                        elif (clickL[0]  == 3 and clickL[1] == 2):
-                            twoClick = True
-                            twobClick = True
-                            threeClick = True
-                            threebClick = True
-                            screen.blit(blank,x[2])
-                            screen.blit(blank,x[3])
-                            screen.blit(blank,x[4])
-                            screen.blit(blank,x[5])
-                            print('inside')
-                        elif (clickL[0]  == 2 and clickL[1] == 4):
-                            fourClick = True
-                            fourbClick = True
-                            twoClick = True
-                            twobClick = True
-                            screen.blit(blank,x[2])
-                            screen.blit(blank,x[3])
-                            screen.blit(blank,x[6])
-                            screen.blit(blank,x[7])
-                        elif (clickL[0]  == 4 and clickL[1] == 2):
-                            fourClick = True
-                            fourbClick = True
-                            twoClick = True
-                            twobClick = True
-                            screen.blit(blank,x[2])
-                            screen.blit(blank,x[3])
-                            screen.blit(blank,x[6])
-                            screen.blit(blank,x[7])
-                        elif (clickL[0]  == 3 and clickL[1] == 4):
-                            fourClick = True
-                            fourbClick = True
-                            threeClick = True
-                            threebClick = True
-                            screen.blit(blank,x[4])
-                            screen.blit(blank,x[5])
-                            screen.blit(blank,x[6])
-                            screen.blit(blank,x[7])
-                        elif (clickL[0]  == 4 and clickL[1] == 3):
-                            fourClick = True
-                            fourbClick = True
-                            threeClick = True
-                            threebClick = True
-                            screen.blit(blank,x[4])
-                            screen.blit(blank,x[5])
-                            screen.blit(blank,x[6])
-                            screen.blit(blank,x[7])
-                        else:
-                            print ('god what have you done')
+                        #resets values after the player misses something
                         click_counter = 0
                         miss =False
-                        print('post miss')
                         del clickL[1]
                         del clickL[0]
-                        
+            #alerts the player that they won the game           
             if (SCORE == 4):
                 if (winner):
-                    #pygame.draw.rect(screen,BLUE,(400,1000,100,25))
                     score_board = myfont.render('You Win ', True,(255,255,255))
                     screen.blit(score_board,(400,500))
                     WIN = True
